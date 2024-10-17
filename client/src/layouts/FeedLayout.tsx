cca7a55dea3af43b0ef2880aca6745017ee35fc8
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-10-17 09:50:12 +0300

    d

--- client/src/layouts/FeedLayout.tsx
+++ client/src/layouts/FeedLayout.tsx
@@ -168,9 +168,11 @@ function FeedLayout() {
                                     minW={0}
                                 >
                                     <Avatar
+                                    bg="blue.300"
+                                    name={userName}
                                         size={"sm"}
                                         src={
-                                            "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                                            "https://bit.ly/broken-link"
                                         }
                                     />
                                 </MenuButton>
